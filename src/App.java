70d1be42608a6e58798c70c9736d0e1d50579e4e
Cristina <cristinaeliloja12@gmail.com>
2024-10-28 12:20:24 -0500

    Recursividad

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Recursividad rec = new Recursividad();
+        int resultado = rec.factorial(5);
+        System.out.println(resultado);
+    }
+}
